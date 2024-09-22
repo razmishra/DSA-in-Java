e4c8e4565f64b8d0d637c8fd71e4d5cf869bb5c1
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2024-09-22 10:22:21 +0530

    day-1

---
+++ Hello.java
@@ -0,0 +1,33 @@
+class Hello{
+    public static void main(String[] args) { // arguments
+        // how to declare variable
+
+        // 1. data type
+        // 2. variable's name
+        // 3. value(optional)
+        // int variable1;
+        // System.out.println("Helllo there");
+        // variable1 = 12;
+        // System.out.println(variable1);
+
+        // int variable2 = 100_000_000;
+        // System.out.println(variable2);
+
+        // float variable3 = 2.3413323f;
+        // System.out.println(variable3);
+
+        // char charactes = 'a';
+        // System.out.println(charactes);
+
+        // boolean bool = true;
+        // System.out.println(bool);
+
+        // 5 => 101
+        // int binary = 0x1f;
+        // System.out.println(binary);
+        int num1 = 2;
+        int num2 = 3;
+        int result = num1+num2; // 5
+        System.out.println(result);
+    }
+}
