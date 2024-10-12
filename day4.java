358bf4d2f7879143639b4f1502191bfc5e1a7892
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2024-10-12 10:31:45 +0530

    day 4

---
+++ day4.java
@@ -0,0 +1,60 @@
+class day4{
+    public static void main(String[] args) {
+        // boolean newUser = true;
+        boolean catt = 123>10;
+        if(catt){
+            System.out.println("Hello");
+        }else{
+            System.out.println("Welcome back!");
+        }
+
+    }
+}
+
+// home Work
+
+// # Problem Statement 1:
+// Write a program that classifies a student's grade based on their score. The grading system is as follows:
+// - A: 90 and above
+// - B: 80 to 89
+// - C: 70 to 79
+// - D: 60 to 69
+// - F: below 60
+
+// # Sample Input:
+// Score: 85
+
+// Output 
+// B
+
+// ---------------------------------------------
+// # Problem Statement 2:
+// Write a program that determines a person's age category based on their age. The categories are defined as follows:
+// - Child: 0 to 12 years
+// - Teenager: 13 to 19 years
+// - Adult: 20 to 64 years
+// - Senior: 65 years and above
+
+// # Sample Input:
+// Age: 45
+// Output
+// Adult
+
+// -----------------------------------------------
+// # Problem Statement 3:
+// Create a program that converts a temperature from Celsius to Fahrenheit or from Fahrenheit to Celsius based on user input. 
+//   The user specifies the conversion type-:
+// - Take type "C" for Celsius to Fahrenheit
+// - Take type "F" for Fahrenheit to Celsius
+
+// Formula----
+// F = C * (9 / 5) + 32
+// C = (F - 32) * (5 / 9)
+
+
+// # Sample Input:
+// Temperature: 100
+// Type: C
+
+// Output
+// 37.77
