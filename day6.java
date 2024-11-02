18745cc27c0ad51d57cdd77e4ca33e53c7052f41
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2024-11-02 10:16:09 +0530

    day 6

---
+++ day6.java
@@ -0,0 +1,49 @@
+class day6 {
+    public static void main(String[] args) {
+
+        int a = 0;
+        int b = 12;
+        // boolean res = a > b && b % 2 == 0;
+
+        if (a > b && b % 2 == 0) {
+            System.out.println("A is positive");
+        } else {
+            System.out.println("A is less than 1");
+        }
+
+        // --- dead code
+        // if(true){
+        //     System.out.println("This is if");
+        // }else{
+        //     System.out.println("This is  else");
+        // }
+
+        // int day = 4;
+        // switch(day){
+        //     case 0:
+        //         System.out.println("Sunday");
+        //         break;
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     default:
+        //         System.out.println("Saturday");
+        // }
+
+        // In java 14, switch statements need not to have break statements.
+        // replace : with ->
+
+    }
+}
