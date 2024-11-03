c707d3d8505cc45b4aee0b871f3ded8107d3578a
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2024-11-03 10:13:18 +0530

    day 7

---
+++ day7.java
@@ -0,0 +1,70 @@
+
+class day7 {
+
+    public static void main(String[] args) {
+
+        // // Prompt user for product type
+        // String productType = "Electronics";
+        // String category1 = "Electronics";
+        // int price = 100;
+
+        // // Initialize category variable
+        // String category = "";
+
+        // // Determine category using nested if-else statements
+        // // if (productType.equalsIgnoreCase("Electronics")) {
+        // if (productType == category1) {
+        // if (price >= 0 && price <= 100) {
+        // category = "Budget";
+        // } else if (price >= 101 && price <= 500) {
+        // category = "Mid-Range";
+        // } else if (price >= 501) {
+        // category = "Premium";
+        // } else {
+        // category = "Invalid price";
+        // }
+        // } else if (productType.equalsIgnoreCase("Clothing")) {
+        // if (price >= 0 && price <= 50) {
+        // category = "Budget";
+        // } else if (price >= 51 && price <= 150) {
+        // category = "Mid-Range";
+        // } else if (price >= 151) {
+        // category = "Premium";
+        // } else {
+        // category = "Invalid price";
+        // }
+        // } else if (productType.equalsIgnoreCase("Grocery")) {
+        // if (price >= 0 && price <= 20) {
+        // category = "Budget";
+        // } else if (price >= 21 && price <= 100) {
+        // category = "Mid-Range";
+        // } else if (price >= 101) {
+        // category = "Premium";
+        // } else {
+        // category = "Invalid price";
+        // }
+        // } else {
+        // category = "Invalid product type";
+        // }
+
+        // if (!category.equals("Invalid price") && !category.equals("Invalid product
+        // type")) {
+        // System.out.println("The product is a " + productType + " in the " + category
+        // + " category.");
+        // } else {
+        // System.out.println(category);
+        // }
+
+        int num = 12;
+        // int res; // initilization
+        // if(num<10){
+        // res = 1; // declaration
+        // }else{
+        // res = 0; //declaration
+        // }
+        // System.out.println(res);
+
+        int res = num > 10 ? 1 : 0; // ?: ternary operator
+        System.out.println(res);
+    }
+}
