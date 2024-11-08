e4f804562c6ddac3e0452fb5c629e54aebb2f5c3
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2024-11-08 10:12:53 +0530

    day 8

---
+++ day8.java
@@ -0,0 +1,31 @@
+class day8 {
+    // main block
+    public static void main(String[] args) {
+        // entry point for JDk
+        // int a = 12;
+        // System.out.println(a);
+
+
+        // ----- LOOPs----
+        // System.out.println("Hi");  //1 
+        // System.out.println("Hi"); // 2
+        // System.out.println("Hi");
+        // System.out.println("Hi");
+        // System.out.println("Hi");
+
+        // This will run infinite times
+        // while (true) {
+        //     System.out.println("Hi");
+        // }
+
+
+        // --- 
+        int i = 1;
+        while (i<=3) {
+            System.out.println("Hi");
+            i = i+1;
+        }
+    }
+}
+
+
