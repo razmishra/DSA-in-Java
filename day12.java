3199649c525cc6ba26c6c41ada9441148678d224
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2024-11-30 11:20:35 +0530

    day 12

---
+++ day12.java
@@ -0,0 +1,55 @@
+class day12 {
+    public static void main(String[] args) {
+
+        // for(int i=1;i<=4;i++){
+        // for(int j=1; j<=(4-i+1); j++){
+        // System.out.print("* ");
+        // }
+        // System.out.println("");
+        // }
+        /*
+         * for (int i = 1; i <= 6; i++) {
+         * for (int j = 1; j <= i; j++) {
+         * if ((i + j) % 2 == 0) {
+         * System.out.print("1 ");
+         * } else {
+         * System.out.print("0 ");
+         * }
+         * }
+         * System.out.println("");
+         * }
+         */
+        // for(int i=1;i<=4;i++){
+        // for(int j=1;j<=4-i;j++){
+        // System.out.print(" ");
+        // }
+        // for(int k=1;k<=i;k++){
+        // System.out.print("* ");
+        // }
+        // System.out.println("");
+
+        // }
+
+        // print below pattern
+        // ******
+        // *    *
+        // *    *
+        // *    *
+        // *    *
+        // ******
+    
+        //      1 
+        //     2 2 
+        //    3 3 3 
+        //   4 4 4 4 
+        //  5 5 5 5 5 
+        // 6 6 6 6 6 6
+
+        // 1    
+        // 1 2 
+        // 1 2 3 
+        // 1 2 3 4 
+        // 1 2 3 4 5 
+        // 1 2 3 4 5 6 
+    }
+}
