feec371496d9b7f681989c7707041caf4db88f3c
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2024-12-01 11:05:37 +0530

    day 13

---
+++ day13.java
@@ -0,0 +1,79 @@
+class day13 {
+    public static void main(String[] args) {
+        // print this pattern
+        // 1
+        // 2 3
+        // 4 5 6
+        // 7 8 9 10
+        // 11 12 13 14 15
+        // 16 17 18 19 20 21
+        
+        /*
+         * int row = 7;
+         * int x =1;
+         * for(int i=1; i<=row; i++){
+         * for(int j=1; j<=i; j++){
+         * System.out.print(x + " ");
+         * x++;
+         * }
+         * System.out.println();
+         */
+        // }
+
+        // *
+        // ***
+        // *****
+        // *******
+        // *********
+        // ***********
+
+        // int row = 6;
+        // for (int i = 1; i <= row; i++) {
+        //     for (int k = 1; k <= row - i; k++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= ((2 * i) - 1); j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // *********
+        // *******
+        // *****
+        // ***
+        // *
+
+        /*
+         * int row1=5;
+         * for(int i=1; i<=row1; i++){
+         * for(int j=1;j<=i;j++){
+         * System.out.print(" ");
+         * }
+         * for(int k=1;k<=(2*row1)-((2*i)-1);k++){
+         * System.out.print("*");
+         * }
+         * System.out.println();
+         * }
+         */
+
+        // 1 2 3 4 5 6
+        //  2 3 4 5 6
+        //   3 4 5 6
+        //    4 5 6
+        //     5 6
+        //      6
+        
+        // int n = 6;
+        // for (int i = 1; i <= 6; i++) {
+        //     for (int j = 1; j <= i - 1; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int k = i; k <= n; k++) {
+        //         System.out.print(k + " ");
+        //     }
+        //     System.out.println("");
+        // }
+
+    }
+}
