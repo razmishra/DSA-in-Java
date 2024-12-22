7e976bbd7fe0f8f96f2fa2afdb6e6cf2faed36c9
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2024-12-22 10:23:46 +0530

    day 15

---
+++ day15.java
@@ -0,0 +1,23 @@
+class day15 {
+
+    // public static void printSquare(int row){
+    // for(int i=1; i<=row; i++){
+    // for(int j=1; j<=row; j++){
+    // System.out.print("* ");
+    // }
+    // System.out.println();
+    // }
+    // }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        // printSquare(4);
+        // printSquare(6);
+        // int res = sum(1, 9);
+        // System.out.println(res);
+        // System.out.println(sum(1,7));
+    }
+}
