4f0f2702db1db70d186b3d14142b89ad5218db88
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2025-02-02 10:09:11 +0530

    day 22

---
+++ day22.java
@@ -0,0 +1,30 @@
+import java.util.*;
+// class Student{
+//     String name;
+//     int id;
+//     String ph;
+    
+//     public Student(String name,String ph){
+//         this.name = name;
+//         this.ph = ph;
+//     }
+    
+//     public void studentInfo(){
+//         System.out.println(this.name);
+//         System.out.println(this.ph);
+//     }
+    
+// }
+
+
+class day22{
+    public static void main(String[] args) {
+        // Student st = new Student("hi there","12121212");
+        // st.studentInfo();
+        Scanner sc = new Scanner(System.in);
+        String newStr = sc.nextLine();
+        sc.close();
+        // String str = new String("john");
+        System.out.println(newStr);
+    }
+}
