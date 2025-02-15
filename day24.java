0354de5b31b2afa8422729b6ada361b44385646f
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2025-02-15 10:06:12 +0530

    day 24 String

---
+++ day24.java
@@ -0,0 +1,27 @@
+// import java.util.*;;
+
+class day24{
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // String getStr = sc.next(); // for a single word
+        // String getStr2 = sc.nextLine(); //  for whole sentence
+        // int getInt = sc.nextInt();
+        // System.out.println(getStr);
+        // System.out.println(getStr2);
+        // System.out.println(getInt);
+        // sc.close();
+
+        // ------ functions---------
+        String str1 = "hi";
+        // String str2 = "hi";
+        String str3 = new String("hi");
+        // int num = 12;
+        // System.out.println(num + str2);
+
+        // ---- comparision---
+        // System.out.println(str1==str2);
+        System.out.println(str1.compareTo(str3));
+        // System.out.println(str1.compareToIgnoreCase(str2));
+
+    }
+}
