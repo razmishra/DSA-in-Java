aaee0425c36f3fb9f64ed95dbf874974954fae47
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2025-02-16 10:58:51 +0530

    day 25 string functions

---
+++ day25.java
@@ -0,0 +1,14 @@
+
+class day25 {
+    public static void main(String[] args) {
+        String s = "namee";
+        // for (int i = 0; i<=s.length() -1; i++) {
+        //     System.out.println(s.charAt(i)+" ");
+        // }
+
+        // --- add a new word
+        // System.out.println(s.concat(" xyz"));
+        // System.out.println(s+" xyz");
+        System.out.println(s.replace('e', 'p'));
+    }
+}
