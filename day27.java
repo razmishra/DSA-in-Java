bc16bd776fc819d123caa46c7e1eb57aa71c0daa
rajneesh.mishra <rajneesh.mishra@extramarks.com>
2025-02-27 09:10:56 +0530

    day 27

---
+++ day27.java
@@ -0,0 +1,31 @@
+class A extends Object {
+    public A() {
+        super();
+        System.out.println("in A");
+    }
+
+    public A(int n){
+        super();
+        System.out.println("in A with int");
+    }
+}
+
+class B extends A {
+    public B() {
+        super();
+        System.out.println("in B");
+    }
+
+    public B(int n){
+        // super(n);
+        this();
+        System.out.println("in B with int");
+    }
+
+}
+
+public class day27 {
+    public static void main(String args[]) {
+        B obj = new B(5);
+    }
+}
